219575ed9d09f4df7b23d959e57e439c45dcd3f3
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 23:22:32 +0200

    create getCategories selectors

---
+++ src/redux/getCategories/getCategoriesSelectors.ts
@@ -0,0 +1,26 @@
+import { createSelector } from 'reselect';
+
+interface State {
+  categories: {
+    isFetchingCategories: boolean,
+    categories: string[],
+    error: string | null
+  }
+}
+
+const selectCategories = (state: State) => state.categories;
+
+export const selectIsFetchingCategories = createSelector(
+  [selectCategories],
+  categories => categories.isFetchingCategories
+);
+
+export const selectCategoriesList = createSelector(
+  [selectCategories],
+  categories  => categories.categories
+);
+
+export const selectErrorFetchingCategories = createSelector(
+  [selectCategories],
+  categories  => categories.error
+);
