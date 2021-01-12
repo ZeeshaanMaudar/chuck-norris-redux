db51d9fe44cc7456b8128de61ab67cf7f617f216
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 23:51:42 +0200

    combine randomJoke reducer into rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import categoriesReducer from './getCategories/getCategoriesReducer';
+import randomJokeReducer from './getRandomJoke/getRandomJokeReducer';
 
 const rootReducer = combineReducers({
   categories: categoriesReducer,
+  randomJoke: randomJokeReducer
 });
 
 export default rootReducer;
