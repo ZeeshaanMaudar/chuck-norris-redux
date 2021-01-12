55a88d3638cd923fb372187f7ecea7fc2b996d96
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 22:39:06 +0200

    add colors array util

---
+++ src/utils/colorsArray.ts
@@ -0,0 +1,4 @@
+const colorsArray = ['#B7C2E9', '#D9DAF3', '#D9E7F3', '#D9F3DF', '#D9F3EB', '#D9F3ED', '#D9F3F2', '#E8D9F3', '#EEEFC0', '#F3D9F1', '#F3E4D9', '#F3EAD9', '#F3F3D9', '#F4C8C8', '#F9D7EB', '#FDCFF0'];
+
+export default colorsArray;
+
